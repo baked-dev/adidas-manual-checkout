225aa381508c0e9a35ea23847a610940d57143f0
xhalfbakedguy <Trillo@Trillos-MBP.fritz.box>
2018-04-11 16:12:36 +0200

    fianlly fixed US

--- Classes/CheckoutWindow.js
+++ Classes/CheckoutWindow.js
@@ -12,16 +12,19 @@ class CheckoutWindow extends BrowserWindow{
                 session: session.fromPartition(id.toString())
             }
         })
+        this.filter = [];
         this.region = region;
         this.webContents.session.clearStorageData([], () => {
-            if(cookie.includes('document.cookie')) cookie = cookie.split('"')[1]
-            console.log(cookie);
+            if(cookie.includes('document.cookie')) cookie = cookie.split('"')[1];
             this.webContents.session.cookies.set({url: 'http://www.'+region_data[this.region].base, name: cookie.split('=')[0], value: cookie.split('=')[1]}, (error) => {
                 if(error) throw new Error(error);
-                console.log(`https://www.${region_data[this.region].base}/on/demandware.store/Sites-adidas-${region_data[this.region].countrycode}-Site/${region_data[this.region].lang}/${type==="paypal"?"CODelivery-RedirectToPaypal":"COShipping-Show"}`)
                 this.loadURL(`https://www.${region_data[this.region].base}/on/demandware.store/Sites-adidas-${region_data[this.region].countrycode}-Site/${region_data[this.region].lang}/${type==="paypal"?"CODelivery-RedirectToPaypal":"COShipping-Show"}`)
             })
-        })
+        });
+        this.webContents.session.webRequest.onBeforeSendHeaders(this.filter, (details,callback) => {
+            details.requestHeaders['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36";
+            callback({cancel: false, requestHeaders: details.requestHeaders});
+        });
     }
 }
 
